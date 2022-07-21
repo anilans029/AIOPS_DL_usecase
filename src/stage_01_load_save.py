8526d8332e471a0b55925d3c53e201c6e8b03b00
anil <anilsai029@gmail.com>
2022-07-21 11:56:33 +0530

    stage_01_load_data created

--- src/stage_01_load_save.py
+++ src/stage_01_load_save.py
@@ -1,30 +1,53 @@
+from venv import create
 from src.utils.all_utils import read_yaml, create_directory
 import pandas as pd
 import argparse
 import os
+import shutil
+from tqdm import tqdm
+import logging as lg
 
-def get_config(config_path):
+
+
+
+def copy_files(source_data_dir, local_data_dir):
+    list_files = os.listdir(source_data_dir)
+    no_of_files= len(list_files)
+    for file in tqdm(list_files, total=no_of_files, desc="copying files from source to local_data_folder", colour='red'):
+        src_file = os.path.join(source_data_dir, file)
+        dest_file = os.path.join(local_data_dir,file)
+        shutil.copy(src=src_file,dst=dest_file)
+
+
+def get_data(config_path):
     config = read_yaml(config_path)
-    remote_data_path =config["data_source"]
-    df = pd.read_csv(remote_data_path, sep=";")
 
-### we need to save this data as csv file in the path artifacts/raw_local_dir/data.csv
+    ## getting the directories from the config file
 
-    artifact_dir = config["artifacts"]["artifacts_dir"]
-    raw_local_dir = config["artifacts"]["raw_local_dir"]
-    raw_local_fie = config["artifacts"]["raw_local_fie"]
+    source_data_dir = config["source_data_dir"]
+    local_data_dir = config["local_data_dir"]
 
-    raw_local_dir_path = os.path.join(artifact_dir,raw_local_dir)
-    raw_local_fie_path = os.path.join(raw_local_dir_path,raw_local_fie)
-    print(raw_local_fie_path)
-    create_directory(dirs = [raw_local_dir_path])
-    df.to_csv(raw_local_fie_path, sep = ";", index = False)
 
+    for source_data_dir, local_data_dir in tqdm(zip(source_data_dir,local_data_dir),total=2, desc="list of folders", colour="green"):
+        create_directory([local_data_dir])
+        copy_files(source_data_dir, local_data_dir)
 
 
 if __name__ == "__main__":
-    args = argparse.ArgumentParser()
 
+    
+    logging_format = "[%(asctime)s -- %(module)s -- %(levelname)s] ===> %(message)s"
+    log_dir = "logs"
+    create_directory([log_dir])
+    lg.basicConfig(filename=os.path.join(log_dir,"running_logs.log"),level= lg.INFO, format= logging_format, filemode="a")
+    
+    args = argparse.ArgumentParser()
     args.add_argument("--config", "-c", default="config/config.yaml")
     parse_args = args.parse_args()
-    get_config(parse_args.config)
+    try:
+        lg.info("stage 1 --->  (loading data from source to local) started")
+        get_data(parse_args.config)
+        lg.info("stage 1 --->  (loading data from source to local) completed")
+    except Exception as e:
+        lg.exception(e)
+        raise e
--- src/utils/all_utils.py
+++ src/utils/all_utils.py
@@ -1,23 +1,25 @@
 import yaml
 import os
 import json
+import logging as lg
 
 def read_yaml(path_to_yaml: str)-> dict:
     with open(path_to_yaml) as yaml_file:
         content = yaml.safe_load(yaml_file)
-
+    
+    lg.info(f"loaded the yaml file from {path_to_yaml} and returning")
     return content
 
 def create_directory(dirs: list):
     for dir in dirs:
         os.makedirs(dir, exist_ok=True)
-        print(f"directory created at {dir}")
+        lg.info(f"directory created at {dir}")
 
 def save_data_local(data, data_path, index_status = False):
     data.to_csv(data_path, index= index_status)
+    lg.info(f"saved the dataframe {data} to csv in local at {data_path}")
 
 def save_report(report: dict, report_path: str, indentation= 4):
     with open(report_path, "w") as f:
         json.dump(report,f,indent = indentation)
-    
-    print(f"report created at {report_path}")
+    lg.info(f"report created at {report_path}")
