bab7b1847cc700da1fcc89297ba99e3313e37def
anil <anilsai029@gmail.com>
2022-07-22 16:42:20 +0530

    callbacks_utils file added

---
+++ src/utils/callbacks_utils.py
@@ -0,0 +1,12 @@
+import os
+
+
+
+
+
+def create_and_save_tf_callback(CALLBACKS_DIR_path, tensorboard_log_dir_path):
+    pass
+
+
+def create_and_save_checkpoint_callback(CALLBACKS_DIR_path,CHECKPOINT_DIR_path):
+    pass
