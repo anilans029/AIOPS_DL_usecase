e14bfe3eb3ca7feb5c70362684ce57cfc740cc3a
anil <anilsai029@gmail.com>
2022-07-22 17:25:59 +0530

    stage 4 base file created

---
+++ src/stage_04_train_model.py
@@ -0,0 +1,48 @@
+from venv import create
+from src.utils.all_utils import read_yaml, create_directory
+import pandas as pd
+import argparse
+import os
+import shutil
+from tqdm import tqdm
+import logging as lg
+import io
+
+
+
+
+
+
+def train_model(config_path, params_path):
+    config = read_yaml(config_path)
+    params = read_yaml(params_path)
+    
+    ## getting the directories from the config file
+
+    artifacts = config["artifacts"]
+    artifacts_dir = artifacts["ARTIFACTS_DIR"]
+    callbacks_dir = artifacts["CALLBACKS_DIR"]
+ 
+    
+
+  
+
+if __name__ == "__main__":
+
+    
+    logging_format = "[%(asctime)s -- %(module)s -- %(levelname)s] ===> %(message)s"
+    log_dir = "logs"
+    create_directory([log_dir])
+    lg.basicConfig(filename=os.path.join(log_dir,"running_logs.log"),level= lg.INFO, format= logging_format, filemode="a")
+    
+    args = argparse.ArgumentParser()
+    args.add_argument("--config", "-c", default="config/config.yaml")
+    args.add_argument("--params", "-p", default="params.yaml")
+    parse_args = args.parse_args()
+    try:
+        lg.info("stage 4 --->  started")
+        train_model(parse_args.config,parse_args.params)
+        lg.info("stage 4     --->  completed(training completed and model is saved)")
+    except Exception as e:
+        lg.exception(e)
+        raise e
